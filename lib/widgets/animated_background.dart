9b0c6337a9bdf1c59b92a2488e123cf41dd6a25f
Bubakr75 <boob_s4@hotmail.com>
2026-03-20 00:33:00 +0100

    Update animated_background.dart

--- lib/widgets/animated_background.dart
+++ lib/widgets/animated_background.dart
@@ -1,36 +1,11 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/theme_provider.dart';
 
-class AnimatedBackground extends StatefulWidget {
+class AnimatedBackground extends StatelessWidget {
   final Widget child;
   final bool showParticles;
   const AnimatedBackground({super.key, required this.child, this.showParticles = true});
-
-  @override
-  State<AnimatedBackground> createState() => _AnimatedBackgroundState();
-}
-
-class _AnimatedBackgroundState extends State<AnimatedBackground> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late List<_Particle> _particles;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 20),
-    )..repeat();
-    _particles = List.generate(25, (_) => _Particle());
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +19,6 @@ class _AnimatedBackgroundState extends State<AnimatedBackground> with SingleTick
     final themeProv = context.watch<ThemeProvider>();
     final bgColor = themeProv.backgroundColor;
 
-    // Crée un dégradé basé sur la couleur choisie
     final hsl = HSLColor.fromColor(bgColor);
     final lighter = hsl.withLightness((hsl.lightness + 0.03).clamp(0.0, 1.0)).toColor();
     final darker = hsl.withLightness((hsl.lightness - 0.02).clamp(0.0, 1.0)).toColor();
@@ -61,20 +35,6 @@ class _AnimatedBackgroundState extends State<AnimatedBackground> with SingleTick
             ),
           ),
         ),
-        if (widget.showParticles)
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (context, _) {
-              return CustomPaint(
-                painter: _ParticlePainter(
-                  particles: _particles,
-                  progress: _controller.value,
-                  glowColor: theme.colorScheme.primary,
-                ),
-                size: Size.infinite,
-              );
-            },
-          ),
         Positioned(
           top: -100,
           left: -50,
@@ -92,59 +52,8 @@ class _AnimatedBackgroundState extends State<AnimatedBackground> with SingleTick
             ),
           ),
         ),
-        widget.child,
+        child,
       ],
     );
   }
-}
-
-class _Particle {
-  final double x;
-  final double y;
-  final double size;
-  final double speed;
-  final double opacity;
-  final double phase;
-
-  _Particle()
-      : x = Random().nextDouble(),
-        y = Random().nextDouble(),
-        size = Random().nextDouble() * 3 + 1,
-        speed = Random().nextDouble() * 0.5 + 0.2,
-        opacity = Random().nextDouble() * 0.3 + 0.05,
-        phase = Random().nextDouble() * 2 * pi;
-}
-
-class _ParticlePainter extends CustomPainter {
-  final List<_Particle> particles;
-  final double progress;
-  final Color glowColor;
-
-  _ParticlePainter({
-    required this.particles,
-    required this.progress,
-    required this.glowColor,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (final p in particles) {
-      final yPos = (p.y + progress * p.speed) % 1.0;
-      final xPos = p.x + sin(progress * 2 * pi + p.phase) * 0.02;
-      final opacity = p.opacity * (0.5 + 0.5 * sin(progress * 2 * pi * p.speed + p.phase));
-
-      final paint = Paint()
-        ..color = glowColor.withValues(alpha: opacity.clamp(0.0, 1.0))
-        ..maskFilter = MaskFilter.blur(BlurStyle.normal, p.size * 2);
-
-      canvas.drawCircle(
-        Offset(xPos * size.width, yPos * size.height),
-        p.size,
-        paint,
-      );
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant _ParticlePainter oldDelegate) => true;
 }
