9bb54badc46b3ed9dc8c4e9fe7dd0f5292cc0ee4
Bubakr75 <boob_s4@hotmail.com>
2026-03-21 06:57:28 +0100

    Update notification_service.dart

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -71,10 +71,11 @@ class NotificationService {
           icon: '@mipmap/ic_launcher',
         ),
       ),
-          androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+      androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
       uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
       matchDateTimeComponents: DateTimeComponents.time,
     );
+  }
 
   static Future<void> scheduleFridayScreenTime({
     required Map<String, String> childrenScreenTime,
@@ -104,10 +105,11 @@ class NotificationService {
           styleInformation: BigTextStyleInformation(''),
         ),
       ),
-            androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+      androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
       uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
       matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime,
     );
+  }
 
   static Future<void> cancelDailyReminder() async {
     await _localNotifications.cancel(1001);
