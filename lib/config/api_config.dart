e342358bd7644db53ea5a98841535e9e74432cdf
Bubakr75 <boob_s4@hotmail.com>
2026-04-16 17:02:46 +0200

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,4 +1,4 @@
 // lib/config/api_config.dart
 class ApiConfig {
-  static const String geminiApiKey = 'AIzaSyBzyWQB3qLYtVakVzInkd5Z86882kayssU';
+  static const String geminiApiKey = 'AQ.Ab8RN6LIG992dH47ys72VDEZxivB7EMWZyyE3MVvnDZ38xVE7w';
 }
